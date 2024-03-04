b235bc214b0c3434468ff1a08eaa0ce5828b9463
unknown <sakarim9124@gmail.com>
2024-03-04 07:02:25 +0530

    updated header n logout api implemented

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,40 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../slices/auth/authApiSlice";
+import { logOutUser } from "../slices/auth/authSlice";
 
 const Header = () => {
+  //misc
+  const { userInfo } = useSelector((state) => state.authReducer);
+  const dispatch = useDispatch();
+
+  //queries n mutation
+  const [logout, { isLoading }] = useLogoutMutation();
+
+  //func
+  const handleLogout = async () => {
+    try {
+      await logout().unwrap();
+      dispatch(logOutUser());
+    } catch (err) {
+      console.error("Failed to logout:", err);
+    }
+  };
+
   return (
-    <div>
-      <h1>Header</h1>
-      <h1>Header</h1>
-      <h1>Header</h1>
-      <h1>Header</h1>
-      <h1>Header</h1>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "50%",
+        margin: "30px auto",
+      }}
+    >
+      <strong>{userInfo ? userInfo?.username : ""}</strong>
+      <button onClick={handleLogout} disabled={isLoading}>
+        Logout
+      </button>
     </div>
   );
 };
